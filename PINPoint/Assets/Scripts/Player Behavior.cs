4d54b1a27f4ac2b7ed1047daf323afb07f228449
azathot69 <jacuna98@gmail.com>
2025-09-03 23:40:25 -0700

    Push

---
+++ PINPoint/Assets/Scripts/Player Behavior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Joseph Acuna 9/3/25 11:27pm
+/// 
+/// This script handles the player's movement
+/// </summary>
+public class PlayerBehavior : MonoBehaviour
+{
+    #region Variables
+    public float moveSpeed;
+    public float jumpSpeed;
+    public float gravity;
+    #endregion
+
+    /// <summary>
+    /// Allows the player to control the camera
+    /// </summary>
+    private void CameraControl()
+    {
+
+    }
+
+    /// <summary>
+    /// Moves the player via the given inputs
+    /// </summary>
+    private void Movement()
+    {
+
+
+    }
+}
