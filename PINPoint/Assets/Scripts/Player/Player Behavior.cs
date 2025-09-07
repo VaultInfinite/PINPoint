ce8956b0800b59b06fa3e3bd79c17f6e7371a51a
azathot69 <jacuna98@gmail.com>
2025-09-07 15:13:58 -0700

    Movement Update

--- PINPoint/Assets/Scripts/Player/Player Behavior.cs
+++ PINPoint/Assets/Scripts/Player/Player Behavior.cs
@@ -1,17 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-enum State
-{
-    IDLE,
-    WALK,
-    RUN,
-    JUMP,
-    AIRMOVE
-}
 
 /// <summary>
 /// Joseph Acuna 9/3/25 11:27pm
@@ -26,11 +18,15 @@ public class PlayerBehavior : MonoBehaviour
     public float moveSpeed;
     public float groundDrag;
     public float runSpeed;
+    private float tempSpeed;
 
+    private MeshRenderer mr;
     private Rigidbody rb;
 
     //Movement
     Vector3 moveInput;
+    Vector3 moveDirection;
+    public Transform orientation;
 
     [Header("Ground Check")]
     public float playerHeight;
@@ -47,41 +43,92 @@ public class PlayerBehavior : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        mr = GetComponent<MeshRenderer>();
     }
 
+    private void Start()
+    {
+        //Turn of the renderer so that the player can't see the model
+        mr.enabled = false;
+        rb.freezeRotation = true;
+    }
 
     public void Update()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
+        GetDirection();
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
 
-        rb.AddForce(moveInput * moveSpeed, ForceMode.Force);
+        SpeedLimit();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else rb.drag = 0;
     }
 
 
     /// <summary>
     /// Moves the player via the given inputs
     /// </summary>
-    void OnMovement(InputValue value)
+    public void OnMovement(InputAction.CallbackContext value)
     {
         //moveInput = value.Get<Vector2>();
 
-        moveInput = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
+        moveInput = new Vector3(value.ReadValue<Vector2>().x, 0, value.ReadValue<Vector2>().y);
 
-  
-        playerState = State.WALK;
+        
+    }
+
+    private void GetDirection()
+    {
+        moveDirection = orientation.forward * moveInput.z + orientation.right * moveInput.x;
+    }
+
+    public void OnSprint(InputAction.CallbackContext value)
+    {
+        if (value.performed)
+        {
+            Debug.Log("Im Running!");
+            tempSpeed = moveSpeed;
+            moveSpeed = runSpeed;
+        }else if (value.canceled)
+        {
+            Debug.Log("Im walkin here!");
+            moveSpeed = tempSpeed;
+        }
+
+        
+    }
+
+    /// <summary>
+    /// Controls how fast the player can go
+    /// </summary>
+    private void SpeedLimit()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //Limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            //Calculate the speed it would be
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+
+            //Apply speed limit
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 
     /// <summary>
     /// Makes the player jump
     /// </summary>
-    void OnJump(InputValue value)
+    public void OnJump(InputAction.CallbackContext value)
     {
-        if ((value.isPressed) && readyToJump && grounded)
+        if ((value.performed) && readyToJump && grounded)
         {
             readyToJump = false;
-
-            playerState = State.JUMP;
 
             //Makes player jump the same height
             rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
