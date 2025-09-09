62c327b72d3ec3f5fcb2cc746c6ab4766c45d5d2
hastrong <148916556+hastrong@users.noreply.github.com>
2025-09-09 11:46:23 -0700

    respawn progress

--- PINPoint/Assets/Scripts/DeathFloor.cs
+++ PINPoint/Assets/Scripts/DeathFloor.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class DeathFloor : MonoBehaviour
 {
     public GameObject playerContainer;
-    public GameObject spawnPoint;
-    
+    private GameObject spawnPoint;
+
+    private void Start()
+    {
+        //GetComponent(spawnPoint.transform.position);
+        //spawnPoint = transform.position;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject)
+        if(collision.gameObject.name == "playerContainer")
         {
             //Destroy(playerContainer);
             //I will switch this to play the respawn code once I have made that
-            
+            //transform.position = spawnPoint;
         }
-    }
-
-    public void Respawn()
-    {
-
     }
 }
