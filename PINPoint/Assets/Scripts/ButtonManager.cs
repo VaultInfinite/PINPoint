c6036c44057ba213c75ac7908bb4b95ea79e0b5e
hastrong <148916556+hastrong@users.noreply.github.com>
2025-09-10 15:17:44 -0700

    Start Menu Button Code part 1

---
+++ PINPoint/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Game scene needs to be connected");
+        //SceneManager.LoadScene();
+    }
+
+    public void Controls()
+    {
+        Debug.Log("Controls panel needs to be connected");
+        //SceneManager.LoadScene();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("You quit the game.");
+    }
+}
