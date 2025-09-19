cc651a07b4bacd843ee75000677b3bd768cd05e6
VaultInfinite <vaultinfinite@gmail.com>
2025-09-18 21:25:43 -0700

    Moving of Code
    Behaviour functionality has been relocated to the new playercontroller

--- PINPoint/Assets/Scripts/Player/StateMachine Attempt/PlayerController.Walking.cs
+++ PINPoint/Assets/Scripts/Player/StateMachine Attempt/PlayerController.Walking.cs
@@ -10,9 +10,33 @@ public partial class PlayerController
         {
             //Put Walking movement related code here
 
+            //Move in faced direction
+            player.GetDirection();
+
+            player.moveSpeed = player.tempSpeed;
+
+            //Apply movement to avatar
+            player.rb.AddForce(player.moveDirection.normalized * player.moveSpeed * 10f, ForceMode.Force);
+
+            //Prevent the player from going too fast
+            player.SpeedLimit();
+
+            //Slow player down when on the ground
+            player.rb.drag = player.groundDrag;
+
             if (player.input.Movement.Sprint.IsPressed())
             {
                 player.SetState<Running>();
+            }
+
+            if (player.input.Movement.Jump.IsPressed())
+            {
+                player.SetState<Air>();
+            }
+
+            if (player.input.Movement.Crouch.IsPressed())
+            {
+                player.SetState<Crouch>();
             }
         }
     }
--- PINPoint/Assets/Scripts/Player/StateMachine Attempt/PlayerController.cs
+++ PINPoint/Assets/Scripts/Player/StateMachine Attempt/PlayerController.cs
@@ -6,11 +6,63 @@ using UnityEngine.InputSystem;
 
 public partial class PlayerController : MonoBehaviour
 {
+    
+    #region Variables
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float runSpeed;
+    private float tempSpeed;
+
+    public bool canMove = true;
+
+    bool sprinting;
+    public bool doublejump;
+
+    private MeshRenderer mr;
+    private Rigidbody rb;
+
+    [Header("Direction")]
+    public Transform orientation;
+    Vector3 moveInput;
+    Vector3 moveDirection;
+
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    bool grounded;
+    public float gravity;
+    public float maxVel;
+    public LayerMask Ground;
+    public float jumpForce;
+    public float jumpCD;
+    public float airMulti;
+    bool readyToJump = true;
+
+    //Crouching Code
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchScaleY;
+    private float startScaleY;
+
+    private bool crouching = false;
+
+    //Player State
+    public bool freeze;
+    public bool unlimited;
+    public bool restricted;
+    #endregion
+
     //Dictionary containing all the states the player can be in
     private readonly Dictionary<Type, State> _states = new()
     {
         {typeof(Walking), new Walking() },
         {typeof(Running), new Running() },
+        {typeof(Jump), new Jump() },
+        {typeof(Ledge), new Ledge() },
+        {typeof(Crouch), new Crouch() },
+        {typeof(Air), new Air() },
     };
     
     //The Input system
@@ -23,6 +75,37 @@ public partial class PlayerController : MonoBehaviour
     {
         input = new();
         input.Enable();
+
+        rb = GetComponent<Rigidbody>();
+        mr = GetComponent<MeshRenderer>();
+        tempSpeed = moveSpeed;
+    }
+
+    private void Start()
+    {
+        //Turn of the renderer so that the player can't see the model
+        mr.enabled = true;
+        rb.freezeRotation = true;
+
+        //Finding Size of Player
+        startScaleY = transform.localScale.y;
+    }
+
+    //Unity builtin fixed update
+    private void FixedUpdate()
+    {
+        var state = _states[_state];
+        state.OnFixedUpdate(this);
+        Debug.Log(_state);
+    }
+
+    private void Update()
+    {
+        var state = _states[_state];
+        state.OnUpdate(this);
+
+        //Check if the player is touching the ground
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
     }
 
     //Sets the current state calling OnEnter on new state and OnExit on old state
@@ -42,17 +125,34 @@ public partial class PlayerController : MonoBehaviour
         newState.OnEnter(this);
     }
 
-    //Unity builtin fixed update
-    private void FixedUpdate()
+    /// <summary>
+    /// Makes the player move towards where they are facing
+    /// </summary>
+    private void GetDirection()
     {
-        var state = _states[_state];
-        state.OnFixedUpdate(this);
-        Debug.Log(_state);
+        //Get direction where the player was facing
+        moveDirection = orientation.forward * moveInput.z + orientation.right * moveInput.x;
+
+        //Apply movement in that direction
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 
-    private void Update()
+    /// <summary>
+    /// Controls how fast the player can go
+    /// </summary>
+    private void SpeedLimit()
     {
-        var state = _states[_state];
-        state.OnUpdate(this);
+        //Get velocity from RB
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //Limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            //Calculate the speed it would be
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+
+            //Apply speed limit
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
