6a5eb5ef0cf1cef80837c07c12b323f8b136a2ac
VaultInfinite <148889667+VaultInfinite@users.noreply.github.com>
2025-09-30 17:20:54 -0700

    Grappling work

---
+++ PINPoint/Assets/Scripts/Player/Controller/Grappling.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Grappling : MonoBehaviour
+{
+    private PlayerController player;
+    private Camera cam;
+
+    [SerializeField]
+    private float hookRange;
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float yMultiplier;
+
+    private Vector3 hookPoint;
+
+    private void Awake()
+    {
+        if (gameObject.GetComponent<PlayerController>())
+        {
+            player = gameObject.GetComponent<PlayerController>();
+        }
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        bool hookHit = CanGrapple(out RaycastHit grappleHit);
+        if (player.input.Movement.Shoot.WasPressedThisFrame() && hookHit)
+        {
+            hookPoint = grappleHit.point;
+        }
+        if (player.input.Movement.Shoot.IsPressed() && hookPoint != null && hookHit)
+        {
+            Grapple((hookPoint - player.transform.position).normalized, maxSpeed, acceleration);
+        }
+    }
+
+    private bool CanGrapple(out RaycastHit grappleHit)
+    {
+        bool hookHit = Physics.Raycast(cam.transform.position, cam.transform.forward, out grappleHit, hookRange, player.Ground);
+        return hookHit;
+    }
+
+    private void Grapple(Vector3 moveDirection, float maxSpeed, float acceleration)
+    {
+        Vector3 velocity = player.rb.velocity;
+        float product = Vector3.Dot(moveDirection, velocity);
+        float accel = acceleration * Time.deltaTime;
+        if (product + accel > maxSpeed)
+        {
+            accel = maxSpeed - product;
+        }
+        Vector3 newVelocity = velocity + moveDirection * accel;
+        player.rb.velocity = newVelocity;
+    }
+}
