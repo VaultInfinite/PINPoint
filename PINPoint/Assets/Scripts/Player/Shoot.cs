7c94dc03a39cdee428039918279d9cf2d40c0da2
hastrong <148916556+hastrong@users.noreply.github.com>
2025-10-28 11:28:41 -0700

    shooting patch

--- PINPoint/Assets/Scripts/Player/Shoot.cs
+++ PINPoint/Assets/Scripts/Player/Shoot.cs
@@ -105,7 +105,7 @@ public class Shoot : MonoBehaviour
     //Shoot the bullet
     public void Shooting()
     {
-        AudioControl.Instance.PlaySoundEffect(sfxType.SHOOT);
+        //AudioControl.Instance.PlaySoundEffect(sfxType.SHOOT);
 
         //Check if can shoot
         if (!canShoot) return;
